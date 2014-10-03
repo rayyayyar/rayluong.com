a7899b17b6aa5470c7a29f826265ddeee2103523
Ray Luong <ray@datapad.io>
2014-10-03 00:16:57 -0700

    finish layout on landing and add favicon
    Former-commit-id: 31418dc11ae508700c4796e55f62c0c0c33a9179

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ray Luong</title>
     <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/> 
     <script src="bower_components/modernizr/modernizr.js"></script>
   </head>
--- index.php
+++ index.php
@@ -24,22 +24,23 @@
 
 				<div class="row">
 					<a href="dp-overview.php">
-						<div class="small-5 column">
+						<div class="small-12 medium-push-5 medium-6 medium-offset-1 column">
+							<img src="img/home/datapad.png">
+						</div>
+						<div class="small-12 medium-pull-6 medium-5 column">
 							<h1>DataPad</h1>
 							<p>DataPad was a web app designed for teams to integrate, refine, analyze, explore, and share data. I led design efforts for all key features.</p>
 						</div>
-						<div class="image small-6 small-offset-1 column">
-							<img src="img/home/datapad.png">
-						</div>
+						
 					</a>
 				</div>
 
 				<div class="row">
 					<a href="blippy.php">
-						<div class="small-6 column">
+						<div class="small-12 medium-6 column">
 							<img src="img/home/blippy-screen.png">
 						</div>
-						<div class="image small-5 small-offset-1 column">
+						<div class="image small-12 medium-5 small-offset-1 column">
 							<h1>Blippy Video</h1>
 							<p>Blippy existed as a short-lived web experiment for online video. It allowed people to create their own communities of video and video-watchers. I joined the team in August 2012 to lead all design efforts (UX, visual, research, interaction). It currently continues to live on the web with regular activity.</p>
 						</div>
@@ -48,10 +49,10 @@
 
 				<div class="row">
 					<a href="kickserv.php">
-						<div class="small-6 column">
+						<div class="small-12 medium-6 column">
 							<img src="img/home/kickserv.png">
 						</div>
-						<div class="image small-5 small-offset-1 column">
+						<div class="small-12 medium-5 small-offset-1 column">
 							<h1>Kickserv</h1>
 							<p>Kickserv is a SaaS web application built to help service companies manage sales and operations more efficiently. I worked with the team to prioritize features, create mock-ups, and design brand new interactions.</p>
 						</div>
@@ -60,22 +61,22 @@
 
 				<div class="row">
 					<a href="thinkspace.php">
-						<div class="small-6 column">
+						<div class="small-12 medium-push-5 medium-6 medium-offset-1 column">
+							<img src="img/home/thinkspace.png">
+						</div>
+						<div class="small-12 medium-pull-6 medium-5 column">
 							<h1>Thinkspace</h1>
 							<p>In collaboration with SRI International, our team of four Masters students from Carnegie Mellon University set out to understand the learning dynamics between teachers and students in high school biology classrooms and how SRI's technology could be best applied. In the process, we prototyped an iPad application that helped students solve complex problems.</p>							
-						</div>
-						<div class="image small-5 small-offset-1 column">
-							<img src="img/home/thinkspace.png">
 						</div>
 					</a>
 				</div>
 
 				<div class="row">
 					<a href="ageofwaste.php">
-						<div class="small-6 column">
+						<div class="small-12 medium-6 column">
 							<img src="img/home/ageofwaste-cropped.png">
 						</div>
-						<div class="image small-5 small-offset-1 column">
+						<div class="small-12 medium-5 small-offset-1 column">
 							<h1>Age of Waste</h1>
 							<p>Over the span of two weeks, I kept track of the items I threw away. I extrapolated from this data to see how much trash would be left on Earth after my death (morbidly estimated age). Seeing that some types of materials take longer to decompose than others, it turned out to be an interesting visualization and a useful reminder about my footprint.</p>
 						</div>
