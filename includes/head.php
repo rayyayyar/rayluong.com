9231b0599cbb6b23883c6478244c2a6b8cb5bd1e
Ray Luong <ray@datapad.io>
2014-10-06 15:57:25 -0700

    add GA snippet
    Former-commit-id: 555868f97aed943ce0ffbcef623c64d6ca5f9283

--- includes/head.php
+++ includes/head.php
@@ -7,4 +7,13 @@
     <link rel="stylesheet" href="stylesheets/app.css" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/> 
     <script src="bower_components/modernizr/modernizr.js"></script>
+    <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-26869700-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
   </head>
