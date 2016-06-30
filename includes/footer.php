727bcf2e2a1c13bbf0433070531f8e5b0ee52037
Ray Luong <rayluong@gmail.com>
2016-06-30 08:29:09 -0700

    added to page

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,16 @@
   <div id="footer" class="row text-center tm">
   	<ul>
   		<a href="https://www.linkedin.com/in/ray-luong-44a7457">
-  			<li>li</li>
+  			<li><img src="/img/home/linkedin.svg"></li>
   		</a>
   		<a href="https://dribbble.com/rayluong">
-  			<li>dr</li>
+  			<li><img src="/img/home/dribbble.svg"></li>
   		</a>
   		<a href="https://twitter.com/rayluong">
-  			<li>tw</li>
+  			<li><img src="/img/home/twitter.svg"></li>
   		</a>
   		<a href="https://www.instagram.com/rayluong/">
-  			<li>in</li>
+  			<li><img src="/img/home/instagram.svg"></li>
   		</a>
   	</ul>
   	<div>
