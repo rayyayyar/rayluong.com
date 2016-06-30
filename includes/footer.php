79b9d7bc84569aa6da0ced9cde44d663e9eeac26
Ray Luong <rayluong@gmail.com>
2016-06-30 08:26:23 -0700

    add social icons

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,21 @@
-  <div class="row text-center tm">
+  <div id="footer" class="row text-center tm">
+  	<ul>
+  		<a href="https://www.linkedin.com/in/ray-luong-44a7457">
+  			<li>li</li>
+  		</a>
+  		<a href="https://dribbble.com/rayluong">
+  			<li>dr</li>
+  		</a>
+  		<a href="https://twitter.com/rayluong">
+  			<li>tw</li>
+  		</a>
+  		<a href="https://www.instagram.com/rayluong/">
+  			<li>in</li>
+  		</a>
+  	</ul>
   	<div>
       <small class="light-text">2016<br />♥️, Ray</small>
-     </div>
+    </div>
   </div>
 </body>
 </html>
